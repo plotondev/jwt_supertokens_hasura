31455fc6adc1626a1cf5ffc646c895a4a7d65db3
Hitesh <hitesh@Hiteshs-Mac-mini.local>
2023-03-19 23:37:09 +0530

    code refactor

---
+++ config.ts
@@ -0,0 +1,139 @@
+import ThirdPartyPasswordlessNode from "supertokens-node/recipe/thirdpartypasswordless"
+import { SMTPService } from "supertokens-node/recipe/thirdpartypasswordless/emaildelivery"
+import SessionNode from "supertokens-node/recipe/session"
+import Dashboard from "supertokens-node/recipe/dashboard"
+import UserMetadata from "supertokens-node/recipe/usermetadata"
+import { google } from 'googleapis'
+import { AuthConfig } from "./interfaces";
+const port = process.env.APP_PORT || 3000;
+
+const apiBasePath = "/api/";
+
+export const websiteDomain = process.env.APP_URL || `http://localhost:${port}`;
+
+export const appInfo = {
+    appName: "CareToCall",
+    websiteDomain,
+    apiDomain: websiteDomain,
+    apiBasePath,
+};
+
+export const backendConfig = (): AuthConfig => {
+    let OAuth2 = google.auth.OAuth2
+    let oauth2Client = new OAuth2()
+    return {
+        
+        framework: "express",
+        supertokens: {
+            // this is the location of the SuperTokens core.
+            connectionURI: process.env.SUPERTOKENS_CORE_URI || "http://localhost:3567",
+            apiKey: process.env.SUPERTOKENS_API_KEY || "",
+        },
+        appInfo,
+        // recipeList contains all the modules that you want to
+        // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
+        recipeList: [
+            ThirdPartyPasswordlessNode.init({
+                emailDelivery: {
+                    service: new SMTPService({
+                        smtpSettings: {
+                            host: process.env.SMTP_HOST || '',
+                            authUsername: process.env.SMTP_USER || '',
+                            password: process.env.SMTP_PASS || '',
+                            port: +(process.env.SMTP_PORT || 587),
+                        
+                            from: {
+                                name: process.env.SMTP_FROM_NAME || 'CareToCall Development',
+                                email: process.env.SMTP_FROM_EMAIL || 'info@caretocall.com',
+                            },
+                            secure: false
+                        },
+                    })
+                },
+                override: {
+                    apis: (originalImplementation) => {
+                        return {
+                            ...originalImplementation,
+    
+                            // we override the thirdparty sign in / up API
+                            thirdPartySignInUpPOST: async function (input) {
+                                if (originalImplementation.thirdPartySignInUpPOST === undefined) {
+                                    throw Error("Should never come here")
+                                }
+    
+                                let response = await originalImplementation.thirdPartySignInUpPOST(input)
+    
+                                // if sign in / up was successful...
+                                if (response.status === "OK") {
+                                    // In this example we are using Google as our provider
+                                    
+                                    let accessToken = response.authCodeResponse.access_token
+                                    let userId = response.user.id       
+                                    oauth2Client.setCredentials({access_token: accessToken})
+                                    var oauth2 = google.oauth2({
+                                        auth: oauth2Client,
+                                        version: 'v2'
+                                      })
+                                      oauth2.userinfo.get(
+                                        function(err, res) {
+                                          if (err) {
+                                             console.log(err)
+                                          } else {
+                                            UserMetadata.updateUserMetadata(userId, { first_name: res?.data.given_name,last_name: res?.data.family_name, picture: res?.data.picture, name: res?.data.name})
+                                          }
+                                      })                      
+                                    
+    
+                                    // TODO: ...
+                                }
+    
+                                return response
+                            },
+                        }
+                    }
+                },
+                providers: [
+                    // We have provided you with development keys which you can use for testing.
+                    // IMPORTANT: Please replace them with your own OAuth keys for production use.
+                    ThirdPartyPasswordlessNode.Google({
+                        clientId: process.env.GOOGLE_CLIENT_ID!,
+                        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+                        scope: ["profile", "email"],
+                        
+                    }),
+                ],
+                contactMethod: "EMAIL",
+                flowType: "USER_INPUT_CODE_AND_MAGIC_LINK",
+            }),
+            SessionNode.init({
+                jwt: {
+                    enable: true,
+                },
+                override: {
+                    functions: function (originalImplementation) {
+                        return {
+                            ...originalImplementation,
+                            createNewSession: async function (input) {
+                                input.accessTokenPayload = {
+                                    ...input.accessTokenPayload,
+                                    "https://hasura.io/jwt/claims": {
+                                        "x-hasura-user-id": input.userId,
+                                        "x-hasura-default-role": "user",
+                                        "x-hasura-allowed-roles": ["user"],
+                                    }
+                                };
+    
+                                return originalImplementation.createNewSession(input);
+                            },
+                        };
+                    }
+                },
+            }),
+            UserMetadata.init({}),
+            Dashboard.init({
+                apiKey: process.env.SUPERTOKENS_API_KEY || "",
+            }),
+        ],
+        isInServerlessEnv: true,
+    }
+}
---
+++ dist/config.js
@@ -0,0 +1,135 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.backendConfig = exports.appInfo = exports.websiteDomain = void 0;
+const thirdpartypasswordless_1 = __importDefault(require("supertokens-node/recipe/thirdpartypasswordless"));
+const emaildelivery_1 = require("supertokens-node/recipe/thirdpartypasswordless/emaildelivery");
+const session_1 = __importDefault(require("supertokens-node/recipe/session"));
+const dashboard_1 = __importDefault(require("supertokens-node/recipe/dashboard"));
+const usermetadata_1 = __importDefault(require("supertokens-node/recipe/usermetadata"));
+const googleapis_1 = require("googleapis");
+const port = process.env.APP_PORT || 3000;
+const apiBasePath = "/api/";
+exports.websiteDomain = process.env.APP_URL || `http://localhost:${port}`;
+exports.appInfo = {
+    appName: "CareToCall",
+    websiteDomain: exports.websiteDomain,
+    apiDomain: exports.websiteDomain,
+    apiBasePath,
+};
+const backendConfig = () => {
+    let OAuth2 = googleapis_1.google.auth.OAuth2;
+    let oauth2Client = new OAuth2();
+    return {
+        framework: "express",
+        supertokens: {
+            // this is the location of the SuperTokens core.
+            connectionURI: process.env.SUPERTOKENS_CORE_URI || "http://localhost:3567",
+            apiKey: process.env.SUPERTOKENS_API_KEY || "",
+        },
+        appInfo: exports.appInfo,
+        // recipeList contains all the modules that you want to
+        // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
+        recipeList: [
+            thirdpartypasswordless_1.default.init({
+                emailDelivery: {
+                    service: new emaildelivery_1.SMTPService({
+                        smtpSettings: {
+                            host: process.env.SMTP_HOST || '',
+                            authUsername: process.env.SMTP_USER || '',
+                            password: process.env.SMTP_PASS || '',
+                            port: +(process.env.SMTP_PORT || 587),
+                            from: {
+                                name: process.env.SMTP_FROM_NAME || 'CareToCall Development',
+                                email: process.env.SMTP_FROM_EMAIL || 'info@caretocall.com',
+                            },
+                            secure: false
+                        },
+                    })
+                },
+                override: {
+                    apis: (originalImplementation) => {
+                        return Object.assign(Object.assign({}, originalImplementation), { 
+                            // we override the thirdparty sign in / up API
+                            thirdPartySignInUpPOST: function (input) {
+                                return __awaiter(this, void 0, void 0, function* () {
+                                    if (originalImplementation.thirdPartySignInUpPOST === undefined) {
+                                        throw Error("Should never come here");
+                                    }
+                                    let response = yield originalImplementation.thirdPartySignInUpPOST(input);
+                                    // if sign in / up was successful...
+                                    if (response.status === "OK") {
+                                        // In this example we are using Google as our provider
+                                        let accessToken = response.authCodeResponse.access_token;
+                                        let userId = response.user.id;
+                                        oauth2Client.setCredentials({ access_token: accessToken });
+                                        var oauth2 = googleapis_1.google.oauth2({
+                                            auth: oauth2Client,
+                                            version: 'v2'
+                                        });
+                                        oauth2.userinfo.get(function (err, res) {
+                                            if (err) {
+                                                console.log(err);
+                                            }
+                                            else {
+                                                usermetadata_1.default.updateUserMetadata(userId, { first_name: res === null || res === void 0 ? void 0 : res.data.given_name, last_name: res === null || res === void 0 ? void 0 : res.data.family_name, picture: res === null || res === void 0 ? void 0 : res.data.picture, name: res === null || res === void 0 ? void 0 : res.data.name });
+                                            }
+                                        });
+                                        // TODO: ...
+                                    }
+                                    return response;
+                                });
+                            } });
+                    }
+                },
+                providers: [
+                    // We have provided you with development keys which you can use for testing.
+                    // IMPORTANT: Please replace them with your own OAuth keys for production use.
+                    thirdpartypasswordless_1.default.Google({
+                        clientId: process.env.GOOGLE_CLIENT_ID,
+                        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+                        scope: ["profile", "email"],
+                    }),
+                ],
+                contactMethod: "EMAIL",
+                flowType: "USER_INPUT_CODE_AND_MAGIC_LINK",
+            }),
+            session_1.default.init({
+                jwt: {
+                    enable: true,
+                },
+                override: {
+                    functions: function (originalImplementation) {
+                        return Object.assign(Object.assign({}, originalImplementation), { createNewSession: function (input) {
+                                return __awaiter(this, void 0, void 0, function* () {
+                                    input.accessTokenPayload = Object.assign(Object.assign({}, input.accessTokenPayload), { "https://hasura.io/jwt/claims": {
+                                            "x-hasura-user-id": input.userId,
+                                            "x-hasura-default-role": "user",
+                                            "x-hasura-allowed-roles": ["user"],
+                                        } });
+                                    return originalImplementation.createNewSession(input);
+                                });
+                            } });
+                    }
+                },
+            }),
+            usermetadata_1.default.init({}),
+            dashboard_1.default.init({
+                apiKey: process.env.SUPERTOKENS_API_KEY || "",
+            }),
+        ],
+        isInServerlessEnv: true,
+    };
+};
+exports.backendConfig = backendConfig;
